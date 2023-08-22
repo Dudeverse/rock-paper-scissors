72dd970c61c4f348f6d0b1f8407418afddf587e0
Dudeverse <se352@njit.edu>
2023-08-22 10:30:06 +0530

    first day end

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+
+let x = "ROCK"
+let y = "PAPER"
+
+let z = document.getElementById("first")
+
+z.textContent = "SCISSORS"
+
+let a = document.getElementById("second")
+
+a.textContent = y
+
+// console.log(z)
